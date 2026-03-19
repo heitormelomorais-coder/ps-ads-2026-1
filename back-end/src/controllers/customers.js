e12cfd4f2cdecd1840daf8f3cc37af8797f817cf
heitormelomorais-coder <heitormelomorais@gmail.com>
2026-03-19 12:53:45 +0000

    (05/03) Métodos POST e GET no controller, com testes

--- back-end/src/controllers/customers.js
+++ back-end/src/controllers/customers.js
@@ -33,5 +33,28 @@ controller.create = async function(req, res) {
  }
 }
 
+controller.retrieveAll = async function(req, res) {
+ try {
+   // Recupera todos os registros de clientes, ordenados pelo
+   // campo "name", ascendente
+   const result = await prisma.customer.findMany({
+     orderBy: [ { name: 'asc' }]
+   })
+
+
+   // HTTP 200: OK (implícito)
+   res.send(result)
+ }
+ catch(error) {
+   // Se algo de errado ocorrer, cairemos aqui
+   console.error(error)  // Exibe o erro no terminal
+
+
+   // Enviamos como resposta o código HTTP relativo
+   // a erro interno do servidor
+   // HTTP 500: Internal Server Error
+   res.status(500).end()
+ }
+}
 
 export default controller
--- back-end/src/routes/customers.js
+++ back-end/src/routes/customers.js
@@ -4,6 +4,5 @@ const router = Router()
 
 
 router.post('/', controller.create)
-
-
+router.get('/', controller.retrieveAll)
 export default router
